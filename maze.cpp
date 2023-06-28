312b2ce7330738100b87ae997659388def700afe
John Borin <johnborin03@gmail.com>
2023-06-28 00:36:59 -0400

    maze

---
+++ maze.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <vector>
+#include <sstream>
+#include <string>
+#include <map>
+#include <set>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    cout << "Hello\n";
+}
